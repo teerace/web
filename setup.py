20a9e4c11ef2414e70acb2cc92c03690f63924f0
Krzysztof Socha <ksocha@ksocha.com>
2010-12-04 19:50:37 +0100

    added project skeleton

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+# Use the docstring of the __init__ file to be the description
+DESC = " ".join(__import__('teerace').__doc__.splitlines()).strip()
+
+setup(
+    name = "django-teerace",
+    version = __import__('teerace').get_version().replace(' ', '-'),
+    url = '',
+    author = 'Krzysztof Socha',
+    author_email = '',
+    description = DESC,
+    long_description = read_file('README'),
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires=read_file('requirements.txt'),
+    classifiers = [
+        'License :: OSI Approved :: Apache Software License',
+        'Framework :: Django',
+    ],
+)
---
+++ teerace/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class teeraceTest(TestCase):
+    """
+    Tests for django-teerace
+    """
+    def test_teerace(self):
+        pass
