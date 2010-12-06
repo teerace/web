b22169fac2375dd5afc50f7a359f106752d9b17d
Krzysztof Socha <ksocha@ksocha.com>
2010-12-06 15:24:36 +0100

    added password recovery from django.contrib

--- teerace/accounts/urls.py
+++ teerace/accounts/urls.py
@@ -6,4 +6,15 @@ urlpatterns = patterns('accounts.views',
 	url(r'^logout/$', 'logout', name='logout'),
 	url(r'^register/$', 'register', name='register'),
 	(r'^tour/$', 'first_login'),
+) + patterns('django.contrib.auth.views',
+	(r'^password_reset/$', 'password_reset', {'template_name':
+		'accounts/password_reset.html',
+		'email_template_name': 'accounts/email/password_reset.html'}),
+	(r'^password_reset/done/$', 'password_reset_done', {'template_name':
+		'accounts/password_reset_done.html'}),
+	(r'^password_reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+		'password_reset_confirm', {'template_name':
+		'accounts/password_reset_confirm.html'}),
+	(r'^password_reset/complete/$', 'password_reset_complete',
+		{'template_name': 'accounts/password_reset_complete.html'}),
 )
