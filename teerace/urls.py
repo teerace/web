26dd3514000c2ce4a2967acf4f7ea37e8418cd41
Krzysztof Socha <ksocha@ksocha.com>
2010-12-06 02:56:40 +0100

    added django-admin to urlpatterns

--- teerace/urls.py
+++ teerace/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls.defaults import (patterns, url, include,
 	handler404, handler500)
+from django.contrib import admin
 
 # FIXME add 404/500 handlers
 # handler404 = ''
 # handler500 = ''
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
 	url(r'^$', 'django.views.generic.simple.direct_to_template',
 		{'template': 'home.html'}, name='home'),
 	(r'^user/', include('accounts.urls')),
+	(r'^admin/', include(admin.site.urls)),
 )
