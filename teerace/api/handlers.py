50a328d25f7187218efa696d18f2dda7320634fb
Krzysztof Socha <ksocha@ksocha.com>
2010-12-06 22:30:45 +0100

    added User API handler

--- teerace/api/handlers.py
+++ teerace/api/handlers.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import User
 from piston.handler import BaseHandler
 from race.models import Run
 
 
 class RunHandler(BaseHandler):
 	model = Run
+
+class UserHandler(BaseHandler):
+	model = User
+
--- teerace/api/urls.py
+++ teerace/api/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls.defaults import patterns, url
 from piston.resource import Resource
-from api.handlers import RunHandler
+from api.handlers import RunHandler, UserHandler
 from lib.piston_auth import APIKeyAuthentication
 
 auth = APIKeyAuthentication(realm="Teerace")
 run_resource = Resource(RunHandler, authentication=auth)
+user_resource = RunHandler(UserHandler, authentication=auth)
 
 # 1st revision of API
 urlpatterns = patterns('',
 	url(r'^1/runs/(?P<id>\d+)/$', run_resource),
 	url(r'^1/runs/$', run_resource),
+	url(r'^1/users/(?P<id>\d+)/$', user_resource),
+	url(r'^1/users/$', user_resource),
 )
