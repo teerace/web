f48eeb8ae15b4867dab1e08c3b3002892f032426
Krzysztof Socha <ksocha@ksocha.com>
2010-12-06 19:27:24 +0100

    fixed, so unauthorized requests using API still get request.user object

--- teerace/lib/piston_auth.py
+++ teerace/lib/piston_auth.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.auth.models import AnonymousUser
 from race.models import Server
 
 
@@ -10,7 +10,8 @@ class APIKeyAuthentication(object):
 	def __init__(self, realm='API'):
 		self.realm = realm
 
-	def is_authenticated(self, request):
+	@staticmethod
+	def is_authenticated(request):
 		auth_string = request.REQUEST.get('key', None)
 
 		if not auth_string:
@@ -21,16 +22,15 @@ class APIKeyAuthentication(object):
 
 		try:
 			server = Server.objects.get(api_key=auth_string)
+			request.user = server.maintained_by
+			request.server = server
+			request.throttle_extra = server.id
 		except Server.DoesNotExist:
-			return False
-
-		request.user = server.maintained_by
-		request.server = server
-		request.throttle_extra = server.id
+			request.user = AnonymousUser()
 
 		return not request.user in (False, None, AnonymousUser())
 
 	def challenge(self):
-		resp = HttpResponse("Authorization Required")
+		resp = HttpResponse("{0} API - Authorization Required".format(self.realm))
 		resp.status_code = 401
 		return resp
