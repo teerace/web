b5be5c61506e47017208fbf1b5aa348e60a112aa
Krzysztof Socha <ksocha@ksocha.com>
2010-12-06 15:19:00 +0100

    added some useful stuff (UUIDField and OverwriteStorage)

---
+++ teerace/lib/file_storage.py
@@ -0,0 +1,15 @@
+import os
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+class OverwriteStorage(FileSystemStorage):
+	def get_available_name(self, name):
+		"""
+		Returns a filename that's free on the target storage system, and
+		available for new content to be written to.
+		"""
+		# If the filename already exists, remove it as if it was a true file system
+		if self.exists(name):
+			self.delete(name)
+		return name
