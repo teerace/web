7df919fa09817e4f781e2b5b6a40a3795ddc0e69
Krzysztof Socha <ksocha@ksocha.com>
2010-12-07 18:05:57 +0100

    added API method to validate users added autogenerated API docs

--- teerace/api/handlers.py
+++ teerace/api/handlers.py
@@ -1,11 +1,106 @@
+from datetime import datetime
 from django.contrib.auth.models import User
 from piston.handler import BaseHandler
 from race.models import Run
+from lib.aes import aes_decrypt
 
 
 class RunHandler(BaseHandler):
+	"""
+	Handles posting of Run objects to webapp.
+	"""
+
+	allowed_methods = ('POST',)
 	model = Run
 
-class UserHandler(BaseHandler):
+	@classmethod
+	def resource_uri(cls):
+		return ('api_run_manage', [])
+
+	def create(self, request):
+		"""
+		Create one or more Run objects.
+		
+		To create more than one object, use list:
+			[
+			{...run1...},
+			{...run2...}
+			]
+		"""
+		if request.content_type:
+			data = request.data
+
+			success_count = 0
+			run_count = len(data)
+			for run in data:
+				map_name = run['map']
+				try:
+					map = Map.objects.get(name=map_name)
+				except Map.DoesNotExist:
+					continue
+				user_id = run['user']
+				try:
+					user = User.objects.get(pk=user_id)
+				except User.DoesNotExist:
+					continue
+				Run(
+					map = map,
+					server = request.server,
+					user = user,
+					nickname = run['nickname'],
+					time = float(run['time']),
+					created_at = datetime.utcfromtimestamp(run['created_at'])
+				).save()
+				success_count += 1
+			if success_count:
+				resp = rc.CREATED
+				resp.write(
+					"Successfully stored {0} of {1} submitted runs.".format(
+						success_count,
+						run_count,
+					)
+				)
+			else:
+				resp = rc.BAD_REQUEST
+				resp.write("None of the {0} submitted runs were stored.".format(run_count))
+			return resp
+		else:
+			super(RunHandler, self).create(request)
+
+
+class ValidateUserHandler(BaseHandler):
+	"""
+	Handles user/password checking.
+	
+	Uses AES and base64 to receive password, remember of
+	using your server private key to generate cipher.
+	
+	Example of password encoding:
+		base64_encode(aes_encrypt('password', PRIVATE_KEY))
+	"""
+
+	allowed_methods = ('GET',)
 	model = User
 
+	@classmethod
+	def resource_uri(cls, username=None, encoded_pass=None):
+		username_uri = 'username'
+		encoded_pass_uri = 'encoded_pass'
+		if username:
+			username_uri = username.username
+		if encoded_pass:
+			encoded_pass_uri = encoded_pass
+		return ('api_user_validate', [username_uri, encoded_pass_uri])
+
+	def read(self, request, username, encoded_pass):
+		"""
+		Returns True or False, whether provided
+		username and password pass the test.
+		"""
+		key = request.server.private_key
+		try:
+			password = aes_decrypt(encoded_pass, key)
+			user = User.objects.get(username=username)
+		except (TypeError, User.DoesNotExist):
+			return False
+		return user.check_password(password)
--- teerace/api/urls.py
+++ teerace/api/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls.defaults import patterns, url
+from piston.doc import documentation_view
 from piston.resource import Resource
-from api.handlers import RunHandler, UserHandler
+from api.handlers import RunHandler, ValidateUserHandler
 from lib.piston_auth import APIKeyAuthentication
 
 auth = APIKeyAuthentication(realm="Teerace")
 run_resource = Resource(RunHandler, authentication=auth)
-user_resource = RunHandler(UserHandler, authentication=auth)
+user_resource = Resource(ValidateUserHandler, authentication=auth)
 
 # 1st revision of API
 urlpatterns = patterns('',
-	url(r'^1/runs/(?P<id>\d+)/$', run_resource),
-	url(r'^1/runs/$', run_resource),
-	url(r'^1/users/(?P<id>\d+)/$', user_resource),
-	url(r'^1/users/$', user_resource),
+	#url(r'^1/runs/(?P<id>\d+)/$', run_resource),
+	url(r'^1/runs/$', run_resource, name='api_run_manage'),
+	url(r'^1/users/validate/(?P<username>\w+)/(?P<encoded_pass>.+)/$', user_resource,
+		name='api_user_validate'),
+	# automated documentation
+	url(r'^1/docs/$', documentation_view),
 )
