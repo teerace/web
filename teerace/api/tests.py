e6df1dd628fb97b879a6fe8ae93a124126f982b3
Krzysztof Socha <ksocha@ksocha.com>
2010-12-10 19:42:54 +0100

    finally, added some unittests

--- teerace/api/tests.py
+++ teerace/api/tests.py
@@ -1,22 +1,96 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+import unittest
+from django.contrib.auth.models import User
+from django.test.client import Client
+from django.utils import simplejson
+from race.models import Map, Server
+from lib.aes import aes_encrypt
 
-Replace these with more appropriate tests for your application.
-"""
+class TestCase(unittest.TestCase):
+	def setUp(self):
+		self.client = JsonClient()
+		if not User.objects.count():
+			self.user = User.objects.create_user(
+				username = "test",
+				password = "test",
+				email = "test@test.com",
+			)
+		else:
+			self.user = User.objects.get(pk=1)
+		self.map = Map.objects.get_or_create(
+			name='test',
+			created_by=self.user,
+			author='test author',
+			map_file='uploads/maps/test_fd23d879.map',
+			crc='fd23d879'
+		)[0]
+		self.server = Server.objects.get_or_create(
+			name = "Test server",
+			maintained_by = self.user,
+		)[0]
+		self.extra = {
+			'HTTP_API_AUTH': self.server.public_key,
+		}
 
-from django.test import TestCase
 
-class SimpleTest(TestCase):
-	def test_basic_addition(self):
-		"""
-		Tests that 1 + 1 always equals 2.
-		"""
-		self.failUnlessEqual(1 + 1, 2)
+class JsonClient(Client):
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+	def post(self, path, data={}, content_type='application/json',
+		follow=False, **extra):
+		if content_type == 'application/json':
+			data = simplejson.dumps(data)
+		return super(JsonClient, self).post(path, data, content_type, follow, **extra)
 
->>> 1 + 1 == 2
-True
-"""}
+
+class RunTestCase(TestCase):
+
+	def testAuthSuccess(self):
+		# we intentionally want to receive 405 Not Implemented
+		response = self.client.get('/api/1/runs/', {}, **self.extra)
+		self.assertEqual(response.status_code, 405)
+
+	def testAuthFailure(self):
+		self.extra['HTTP_API_AUTH'] = 'invalid'
+		response = self.client.get('/api/1/runs/', {}, **self.extra)
+		self.assertEqual(response.status_code, 401)
+
+	def testCreateRunSuccess(self):
+		data = {
+			'map_name': self.map.name,
+			'user_id': self.user.id,
+			'nickname': "[GER] Ueber Tester",
+			'time': 14.72,
+			'created_at': "2010-12-10 16:53:20",
+		}
+		response = self.client.post('/api/1/runs/', data, **self.extra)
+		self.assertEqual(response.status_code, 201)
+
+	def testCreateRunFailure(self):
+		data = {
+			'map_name': "wrong_map",
+			'user_id': self.user.id,
+			'nickname': "[AUS] Lame Badass",
+			'time': 151.02,
+		}
+		response = self.client.post('/api/1/runs/', data, **self.extra)
+		self.assertEqual(response.status_code, 400)
+
+
+class UserTestCase(TestCase):
+
+	def testValidateUserSuccess(self):
+		data = {
+			'username': "test",
+			'password': "test",
+		}
+		data['password'] = aes_encrypt(data['password'], self.server.private_key)
+		response = self.client.post('/api/1/users/validate/', data, **self.extra)
+		self.assertTrue(simplejson.loads(response.content))
+
+	def testValidateUserFailure(self):
+		data = {
+			'username': "test",
+			'password': "toast", # WHOOOPS, A TYPO!
+		}
+		data['password'] = aes_encrypt(data['password'], self.server.private_key)
+		response = self.client.post('/api/1/users/validate/', data, **self.extra)
+		self.assertFalse(simplejson.loads(response.content))
