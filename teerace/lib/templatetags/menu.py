37f6e391094e4a2b22afae604b8c32e85704d2e0
Krzysztof Socha <ksocha@ksocha.com>
2010-12-12 19:04:24 +0100

    removed oauth templates, moved templatetags to lib

---
+++ teerace/lib/templatetags/menu.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def current(request, pattern):
+	if pattern == "/":
+		if request.path == pattern:
+			return ' id="current"'
+		else:
+			return ''
+	if re.search(pattern, request.path):
+		return ' id="current"'
+	return ''
+
+
+@register.simple_tag
+def current_unique(request, pattern, id):
+	if re.search(pattern, request.path):
+		return ' id="%s-current"' % id
+	return ' id="%s"' % id
