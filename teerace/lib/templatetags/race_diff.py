a2ce75bb809c67b09c3e8892e4c2c51442ecf591
Krzysztof Socha <ksocha@ksocha.com>
2010-12-14 18:00:44 +0100

    added some templatetags, added better listing to map_detail

---
+++ teerace/lib/templatetags/race_diff.py
@@ -0,0 +1,25 @@
+import re
+from django import template
+from race.models import Run
+
+register = template.Library()
+
+@register.simple_tag
+def race_diff(run, compare_to=None, custom_green=None):
+	if run == compare_to:
+		# sneaky, we use it for race_diff(run, custom_green)
+		compare_to = None
+	if compare_to is None:
+		user = run.user
+		map = run.map
+		best_result = Run.objects.filter(map=map, user=user).order_by('time')[0]
+		diff = run.time - best_result.time
+	else:
+		diff = run.time - compare_to.time
+	if diff > 0.0:
+		style = 'red'
+	else:
+		style = 'green'
+		if custom_green:
+			return custom_green
+	return '<span class="{0}">{1:+.2f}</span>'.format(style, diff)
--- teerace/race/views.py
+++ teerace/race/views.py
@@ -1,5 +1,6 @@
 import datetime
 from django.contrib.auth.models import User
+from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list, object_detail
@@ -15,6 +16,11 @@ def homepage(request):
 	except Entry.DoesNotExist:
 		latest_entry = None
 
+	if request.user.is_authenticated():
+		user_runs = Run.objects.filter(user=request.user).order_by('-created_at')[:5]
+	else:
+		user_runs = None
+
 	today = datetime.date.today()
 	yesterday = today - datetime.timedelta(days=1)
 
@@ -25,14 +31,18 @@ def homepage(request):
 		(datetime.datetime.combine(yesterday, datetime.time.min),
 		datetime.datetime.combine(yesterday, datetime.time.max)))
 
+	total_playtime = Run.objects.aggregate(Sum('time'))['time__sum']
+
 	template = 'home.html'
 	extra_context = {
 		'latest_entry': latest_entry,
-		'users': UserProfile.objects.all().select_related(),
-		'maps': Map.objects.all(),
+		'users': list(UserProfile.objects.select_related()),
+		'maps': list(Map.objects.all()),
+		'user_runs': user_runs,
 		'run_count': Run.objects.count(),
 		'runs_today': runs_today,
 		'runs_yesterday': runs_yesterday,
+		'total_playtime': total_playtime,
 	}
 	return direct_to_template(request, template, extra_context)
 
@@ -48,7 +58,7 @@ def map_detail(request, map_id):
 	best_runs = Run.objects.filter(map=map).order_by('time')[:5]
 	latest_runs = Run.objects.filter(map=map).order_by('-created_at')[:5]
 	if request.user.is_authenticated():
-		user_runs = Run.objects.filter(user=request.user)[:5]
+		user_runs = Run.objects.filter(user=request.user).order_by('-created_at')[:5]
 	else:
 		user_runs = None
 	return {
