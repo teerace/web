5c4f40c37686055c162bec32ae47ded2dd4ba8fe
Krzysztof Socha <ksocha@ksocha.com>
2010-12-28 02:57:01 +0100

    objectified AES tools

--- teerace/lib/aes.py
+++ teerace/lib/aes.py
@@ -1,4 +1,4 @@
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES as Cipher_AES
 from django.core.cache import cache
 import base64
 
@@ -7,48 +7,31 @@ Based on:
 http://www.codekoala.com/blog/2009/aes-encryption-python-using-pycrypto/
 """
 
-# the block size for the cipher object; must be 16, 24, or 32 for AES
-BLOCK_SIZE = 32
+class AES(object):
+	# the block size for the cipher object; must be 16, 24, or 32 for AES
+	BLOCK_SIZE = 32
 
-# the character used for padding--with a block cipher such as AES, the value
-# you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
-# used to ensure that your value is always a multiple of BLOCK_SIZE
-PADDING = '{'
+	# the character used for padding--with a block cipher such as AES, the value
+	# you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
+	# used to ensure that your value is always a multiple of BLOCK_SIZE
+	PADDING = '{'
 
-# one-liner to sufficiently pad the text to be encrypted
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+	def __init__(self, key):
+		self.key = key		
+		self._generate_cipher()
 
-def aes_helper(function, string, key):
-	"""
-	Base function for aes_encrypt and aes_decrypt
-	"""
+	def pad(self, string):
+		""" one-liner to sufficiently pad the text to be encrypted """
+		return string + (self.BLOCK_SIZE - len(string) % self.BLOCK_SIZE) * self.PADDING
 
-	if len(key) != BLOCK_SIZE:
-		raise TypeError("Length of key used to generate"
-			" cipher have to match BLOCK_SIZE")
+	def _generate_cipher(self):
+		if len(self.key) != self.BLOCK_SIZE:
+			raise TypeError("Length of the key used to generate"
+				" a cipher have to match BLOCK_SIZE")
+		self.cipher = Cipher_AES.new(self.key)
 
-	cipher = AES.new(key)
+	def decrypt(self, string):
+		return self.cipher.decrypt(base64.b64decode(string)).rstrip(self.PADDING)
 
-	return function(cipher, string)
-
-
-def aes_encrypt(string, key):
-	"""
-	Encrypts `string` using cipher generated from `key`
-	AND encodes result with base64.
-	"""
-
-	encode_aes = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-
-	return aes_helper(encode_aes, string, key)
-
-
-def aes_decrypt(string, key):
-	"""
-	Decodes encoded `string` with base64 and decrypts
-	result using cipher generated from `key`.
-	"""
-
-	decode_aes = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-
-	return aes_helper(decode_aes, string, key)
+	def encrypt(self, string):
+		return base64.b64encode(self.cipher.encrypt(self.pad(string)))
