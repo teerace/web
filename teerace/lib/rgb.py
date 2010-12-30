a4fe3304ba886e0da008621516dd5ea1167d2e30
Krzysztof Socha <ksocha@ksocha.com>
2010-12-30 21:04:14 +0100

    added rgblong -> hex magic

--- teerace/lib/rgb.py
+++ teerace/lib/rgb.py
@@ -3,11 +3,14 @@ Converts RGB and long RGB values to hexadecimal colours
 Awesome work of Robert Chmielowiec.
 """
 
-def rgb_to_rgblong (r, g, b):
+def rgb_to_rgblong(r, g, b):
 	return r * pow (256, 2) + g * 256 + b
 
-def rgblong_to_rgb (rgb):
+def rgblong_to_rgb(rgb):
 	return rgb / pow (256, 2), (rgb & 65535 ^ 255) / 256, rgb & 255
 
 def rgb_to_hex(r, g, b):
 	return "#%02x%02x%02x" % (r, g, b)
+
+def rgblong_to_hex(rgb):
+	return rgb_to_hex(*rgblong_to_rgb(rgb))
