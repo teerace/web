df7fa9489cf0ba36d7d0d780e43abbfeb1941583
Krzysztof Socha <ksocha@ksocha.com>
2010-12-30 22:04:25 +0100

    removed unused import

--- teerace/race/views.py
+++ teerace/race/views.py
@@ -2,7 +2,6 @@ import datetime
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.shortcuts import get_object_or_404, redirect
-from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list
 from accounts.models import UserProfile
 from blog.models import Entry
