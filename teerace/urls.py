db35db50426f261e96ab09a71acc39930a3611e5
Robert Chmielowiec <robert@chmielowiec.net>
2011-02-01 22:38:14 +0100

    added error pages (closes #36)

--- teerace/urls.py
+++ teerace/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.defaults import patterns, url, include, handler404, handler500
 from django.contrib import admin
 import settings
 
