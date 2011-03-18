5966f21beafb640e5a3c7070ffb382806ab6017f
Krzysztof Socha <ksocha@ksocha.com>
2011-03-18 22:39:35 +0100

    obvious bug is obvious

--- teerace/api/handlers.py
+++ teerace/api/handlers.py
@@ -39,8 +39,8 @@ class RunHandler(BaseHandler):
 		if 'id' in kwargs:
 			map_obj = Map.objects.get(pk=kwargs['id'])
 			return BestRun.objects.exclude(points=0) \
-				.filter(map=map_obj).select_related()[:len(BestRun.SCORING)] \
-				.order_by('-points')
+				.filter(map=map_obj).select_related() \
+				.order_by('-points')[:len(BestRun.SCORING)]
 		return rc(rcs.BAD_REQUEST)
 
 	def read(self, request, action, *args, **kwargs):
