b377b8255f362808f2e236ab18283b7c9388c229
Krzysztof Socha <ksocha@ksocha.com>
2011-04-24 16:41:58 +0200

    changed the revision hook script to use arguments provided by git

--- teerace/lib/revision_hook.py
+++ teerace/lib/revision_hook.py
@@ -2,11 +2,14 @@
 # coding: utf-8
 
 from django.core.management import setup_environ
+import settings
+setup_environ(settings)
 from django.core.cache import cache
 from subprocess import Popen, PIPE
-from teerace import settings
-setup_environ(settings)
 
-cmd = ["git", "rev-list", "-n 1", "--first-parent", "master"]
-rev = Popen(cmd, stdout=PIPE).communicate()[0].strip()
+if len(sys.argv) > 2:
+	rev = sys.argv[2]
+else:
+	cmd = ["git", "rev-list", "-n 1", "--first-parent", "master"]
+	rev = Popen(cmd, stdout=PIPE).communicate()[0].strip()
 cache.set('current_revision', rev, 0)
