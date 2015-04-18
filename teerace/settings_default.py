aa13ce1c255ccd821ada1983f7c7214d0c48ac69
SushiTee <weichel.sascha@xxxx.xx>
2015-04-18 17:26:43 +0200

    we need APP_DIRS

--- teerace/settings_default.py
+++ teerace/settings_default.py
@@ -215,6 +215,7 @@ MIDDLEWARE_CLASSES = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
         'DIRS': [
             os.path.join(PROJECT_DIR, 'templates'),
             os.path.join(PROJECT_DIR, 'templates/piston'),
